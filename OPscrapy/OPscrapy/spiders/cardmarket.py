d8c360893f51d03f0c3ae024cd6076a5e0cb9c86
Gerard Comas Quiles <gerard.comas@estudiantat.upc.edu>
2023-11-07 15:29:02 +0100

    Created git_ignore

--- OPscrapy/OPscrapy/spiders/cardmarket.py
+++ OPscrapy/OPscrapy/spiders/cardmarket.py
@@ -1,10 +1,9 @@
 import scrapy
 
-
 class CardmarketSpider(scrapy.Spider):
     name = "cardmarket"
     allowed_domains = ["cardmarket.com"]
-    start_urls = ["https://cardmarket.com"]
+    start_urls = ["https://www.cardmarket.com/en/OnePiece/Products/Singles?idCategory=1621&idExpansion=0&idRarity=0&sortBy=collectorsnumber_asc"]
 
     def parse(self, response):
         pass
